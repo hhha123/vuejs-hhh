967696cffd7d82328e3e696c6c1d5658137ae958
hhha <2453174364@qq.com>
2018-03-11 16:28:13 +0800

    项目

---
+++ my-flower/src/components/Index.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+      <img src="../imgs/logo.png" class="logo" />
+      <div class="header_right">
+	      <a href="#" class="search">
+	      	<span></span>白色情人节鲜花
+	      </a>
+	      <div class="login"><a href="#">登录</a></div>
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style src="../Index.css">
+</style>
