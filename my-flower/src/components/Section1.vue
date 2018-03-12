a803a6b559e8ee393cfeb978a7ba8e4251768416
hhha <2453174364@qq.com>
2018-03-12 10:33:42 +0800

    轮播

---
+++ my-flower/src/components/Section1.vue
@@ -0,0 +1,37 @@
+<template>
+  <section>
+	  <!--<div class="swiper-container">
+		  <div class="swiper-wrapper">
+		    <div class="swiper-slide"><a href="#"><img src="../imgs/slide1.jpg"/></a></div>
+		    <div class="swiper-slide"><a href="#"><img src="../imgs/slide2.jpg"/></a></div>
+		    <div class="swiper-slide"><a href="#"><img src="../imgs/slide3.jpg"/></a></div>
+		  </div>
+		  <div class="swiper-pagination"></div>
+		</div>-->
+		<mt-swipe :auto="4000">
+		  <mt-swipe-item><a href="#"><img src="../imgs/slide1.jpg"/></a></mt-swipe-item>
+		  <mt-swipe-item><a href="#"><img src="../imgs/slide2.jpg"/></a></mt-swipe-item>
+		  <mt-swipe-item><a href="#"><img src="../imgs/slide3.jpg"/></a></mt-swipe-item>
+		</mt-swipe>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Section1',
+  data () {
+    return {
+    }
+  },
+  mounted() {
+//	var mySwiper = new Swiper('.swiper-container',{
+//			  pagination: {
+//			    el: '.swiper-pagination',
+//			  }
+//		})
+  }
+}
+</script>
+
+<style src="../css/Section1.css">
+</style>
--- my-flower/src/router/index.js
+++ my-flower/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Index from '@/components/Index'
+import Section1 from '@/components/Section1'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Index',
-      component: Index
+      name: 'Section1',
+      component: Section1
     }
   ]
 })
