fde6f6837b96bf9c06f10e697c11db060378c86e
hhha <2453174364@qq.com>
2018-03-14 11:14:07 +0800

    详情

--- my-flower/src/components/Section3.vue
+++ my-flower/src/components/Section3.vue
@@ -1,24 +1,53 @@
 <template>
   <div id="shopcar">
   	<div class="shopcar_top">
-  		<img src="../../static/imgs/headChevronLeft.png" alt="" />
-  		<span>购物车</span>
-  		<img src="../../static/imgs/headerIconMore.png" alt="" />
+  		<p>购物车</p>
+  		<a class="icon-arrow-left" @click="back()"></a>
+			<span class="icon-menu"></span>
   	</div>
   	<div class="box">
   		<img src="../../static/imgs/empty_cart3.png" alt="" />
   		<p>别让你的心意空空如也</p>
-  		<a href="">去挑选礼物</a>
+  		<a href="#/">去挑选礼物</a>
   	</div>
+  	<div class="shopTitle">
+  		<span>为你推荐</span>
+  	</div>
+    <div class="content">
+    	 <ul>
+    	 	<li v-for="item in list">
+    	 		<a href="">
+    	 			<img src="../imgs/220x240.jpg"/>
+    	 			<p></p>
+    	 			<font></font>
+    	 		</a>
+    	 	</li>
+    	 </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   name: 'Shopcar',
   data () {
     return {
+    	flag: "false",
+    	list: []
     }
+  },
+  methods: {
+  	back(){
+  		window.history.back();
+  	}
+  },
+  mounted() {
+  	  axios.get("http://localhost:3000/proList")
+	    .then((res)=>{
+	      console.log(res);
+	      this.list = res.data;
+	    })
   }
 }
 </script>
---
+++ my-flower/src/components/list.vue
@@ -0,0 +1,111 @@
+<template>
+	<div id="list">		
+	  <div class="top">
+	  	 <img src="../../static/imgs/headChevronLeft.png" alt="" />  
+	  	 <img src="../../static/imgs/logo@2x.png" alt="" />  
+	  	 <img src="../../static/imgs/headerIconMore.png" alt="" />
+	  </div>
+		<div class="nav">			
+				<a href="">综合</a>
+				<a href="">销量</a>
+				<a href="">价格</a>
+				<a href="">最新</a>			
+		</div>	
+		<div class="tab_nav">
+			<span>全部</span>
+			<span>爱情鲜花</span>
+			<span>友情鲜花</span>
+			<span>送长辈鲜花</span>
+			<span>
+				鲜花分类
+				<i></i>
+			</span>
+		</div>
+		<ul>
+			<li v-for="item in list">
+				<a href="">
+					<img :src="'../../static/imgs/'+item.src" alt="" />
+					<div>						
+						<p>{{item.p_top}}</p>
+						<p>{{item.p_center}}</p>
+						<p>￥{{item.p_bottom}}</p>
+					</div>
+				</a>
+			</li>
+		</ul>
+	</div>	
+</template>
+
+<script>
+export default {
+  name: 'list',
+  data () {
+    return {
+    	list:[
+    		{
+    			src:"9010966.jpg_220x240.jpg",
+    			p_top:"一往情深",
+    			p_center:"精品玫瑰礼盒：19枝红玫瑰，勿忘我适量",
+    			p_bottom:"245"
+    		},
+    		{
+    			src:"9010011.jpg_220x240.jpg",
+    			p_top:"一心一意",
+    			p_center:"玫瑰11枝，粉色勿忘我0.3扎",
+    			p_bottom:"139"
+    		},
+    		{
+    			src:"9012089.jpg_220x240.jpg",
+    			p_top:"爱在心头",
+    			p_center:"玫瑰50枝：戴安娜粉玫瑰19枝，红玫瑰31枝",
+    			p_bottom:"389"
+    		},
+    		{
+    			src:"9010947.jpg_220x240.jpg",
+    			p_top:"真爱如初",
+    			p_center:"雪山玫瑰11枝、深紫色勿忘我0.3枝",
+    			p_bottom:"206"
+    		},
+    		{
+    			src:"9012177.jpg_220x240.jpg",
+    			p_top:"不变的承诺",
+    			p_center:"99枝红玫瑰",
+    			p_bottom:"679"
+    		},
+    		{
+    			src:"9012154.jpg_220x240.jpg",
+    			p_top:"甜美公主",
+    			p_center:"白玫瑰22枝，粉佳人粉玫瑰14枝，粉色桔梗5枝",
+    			p_bottom:"378"
+    		},
+    		{
+    			src:"9012223.jpg_220x240.jpg",
+    			p_top:"我只钟情你",
+    			p_center:"香槟玫瑰11枝、白色小雏菊3枝",
+    			p_bottom:"239"
+    		},
+    		{
+    			src:"9012009.jpg_220x240.jpg",
+    			p_top:"忘情巴黎",
+    			p_center:"33枝红玫瑰",
+    			p_bottom:"298"
+    		},
+    		{
+    			src:"9012092.jpg_220x240.jpg",
+    			p_top:"母爱",
+    			p_center:"紫红色康乃馨9枝，粉色多头康乃馨10枝",
+    			p_bottom:"178"
+    		},
+    		{
+    			src:"9010969.jpg_220x240.jpg",
+    			p_top:"你爱珍贵",
+    			p_center:"精品玫瑰礼盒：香槟玫瑰19枝，勿忘我适量",
+    			p_bottom:"258"
+    		}
+    	]
+    }
+  }
+}
+</script>
+<style src="../../css/common.css"></style>
+<style src="../../css/list.css" scoped></style>
